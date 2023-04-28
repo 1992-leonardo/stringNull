e0d8233c0323e8071688b00c1ad6b21c4369aabb
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-04-27 22:39:58 -0300

    criando minha string

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+   std::string nome = "";
+
+   while (nome.empty()) {
+      std::cout << "Digite um nome: ";
+      std::getline(std::cin, nome);
+
+      if (nome.empty()) {
+         std::cout << "Voce nao digitou nenhum nome. Tente novamente." << std::endl;
+      } else {
+         std::cout << "O nome digitado foi: " << nome << std::endl;
+      }
+   }
+
+   return 0;
+}

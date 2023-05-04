c0210bcc91745a197d0b93867bb737fd241d0b2e
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-05-03 21:37:17 -0300

    Nova linha.

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@ int main() {
          std::cout << "O nome digitado foi: " << nome << std::endl;
       }
    }
-   
-
+         std::cout << "Oi eu sou uma linha. " << std::endl;
    return 0;
 }
